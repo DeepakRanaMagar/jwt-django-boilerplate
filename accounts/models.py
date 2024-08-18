2ae429f9a7bab2728fff9320a85c16f4000ce1b0
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-08-18 12:37:40 +0545

    email backend

--- accounts/models.py
+++ accounts/models.py
@@ -1,55 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 
 '''
     Extending the user model using AbstractBaseUser
 '''
-
-class CustomUserManager(BaseUserManager):
-    '''
-        Handles schema for the Custom user manager, which extends from BaseUserManager
-    '''
-    def create_user(self, phone_no, password=None):
-        if not phone_no:
-            raise ValueError('Users must have a phone number')
-        user = self.model(phone_no=phone_no)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, phone_no, password):
-        user = self.create_user(phone_no, password)
-        user.is_admin = True
-        user.save()
-        return user
-
-class Account(AbstractBaseUser):
-    '''
-        Handles the schema for the Account model
-    '''
-    phone_no = models.IntegerField(_("Phone Number"), unique=True)
-    name = models.CharField(_("Name"), max_length=50)
-    joined_date = models.DateTimeField(_("joined data"), auto_now_add=True)
-
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-    
-    
-    # calling the CustomUserManager()
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'phone_no'
+class Account(AbstractUser):
+    name = models.CharField(_("full name"), max_length=50)
+    email = models.EmailField(_("email"), max_length=254, unique=True)
+    phone = models.IntegerField(_("phone_num"), unique=True)
 
     def __str__(self):
         return self.name
-
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
-
-    @property
-    def is_staff(self):
-        return self.is_admin
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'phone']
+    
