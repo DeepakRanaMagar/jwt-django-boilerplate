27f0209c9f2e2f617c5e03c6c0f5f30c545831b0
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-08-21 17:39:43 +0545

    fix(view): list

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,4 +4,4 @@ from .models import Accounts
 
 @admin.register(Accounts)
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ['email', 'name', 'address', 'phone_no', 'created_at', 'modified_at']
+    list_display = ['email', 'name', 'address', 'phone_no', 'is_admin', 'created_at', 'modified_at']
