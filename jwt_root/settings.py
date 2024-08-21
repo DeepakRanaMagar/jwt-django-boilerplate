c6dcff6cc2e61f8a1a4bc1740e1f1f76eeabfd8c
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-08-21 18:39:19 +0545

    fix(rest_framework): auth classes & permission classes

--- jwt_root/settings.py
+++ jwt_root/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
 from datetime import timedelta
-
+from pathlib import Path
 
 AUTH_USER_MODEL = 'accounts.Accounts'
 # AUTHENTICATION_BACKENDS=["accounts.backend.EmailPhoneAuthenticationBackend"]
@@ -50,12 +49,12 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    },
-    "DEFAULT_PERMISSION_CLASSES": {
-        "rest_framework.permissions.IsAuthenticated",
-    }
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ]
 }
 
 SIMPLE_JWT = {
